9f343394e490da58beaf37eaa0b1c5bf7a258d73
Your Name <you@example.com>
2022-10-24 22:53:03 -0500

    falta arreglar arreglo de polygons

--- hm/settings.py
+++ hm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "hm",
         'USER': 'root',  
-        'PASSWORD': 'hola123',  
+        'PASSWORD': 'root',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
